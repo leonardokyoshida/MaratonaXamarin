9b6848c7a751e889433e36b60e55208713ab8d32
Leonardo Kenji <yoshidak.leonardo@outlook.com>
2017-11-06 15:20:51 -0200

    Aula de Binding finalizada

--- MaratonaXamarin/MaratonaXamarin/MainPage.xaml.cs
+++ MaratonaXamarin/MaratonaXamarin/MainPage.xaml.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MaratonaXamarin.ViewModels;
 using Xamarin.Forms;
 
 namespace MaratonaXamarin
 {
-	public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+            BindingContext = new MainViewModel();
 		}
 	}
 }
---
+++ MaratonaXamarin/MaratonaXamarin/ViewModels/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+
+namespace MaratonaXamarin.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _descricao;
+        public string Descricao
+        {
+            get
+            {
+                return _descricao;
+            }
+            set
+            {
+                SetProperty(ref _descricao, value);
+            }
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                SetProperty(ref _nome, value);
+            }
+        }
+
+        public MainViewModel()
+        {
+            Descricao = "Olá mundo, estou aqui";
+
+            Task.Delay(3000).ContinueWith(t =>
+            {
+                Descricao = "Mudou o texto";
+            });
+        }
+
+        
+    }
+}
