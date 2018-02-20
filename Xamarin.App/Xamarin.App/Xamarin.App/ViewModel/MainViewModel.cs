ee2403e8e1f0a1bbfe31bee32bac808ab23fc55b
Leonardo Kenji Yoshida <yoshidak.leonardo@outlook.com>
2018-02-20 02:36:41 -0300

    Recomeçando 2018

---
+++ Xamarin.App/Xamarin.App/Xamarin.App/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Xamarin.App.ViewModel
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.Descricao = "Maratona Xamarin!! Welcome";
+        }
+        public string Descricao { get; set; }
+    }
+}
