c8ab0727ea1786c69e07384d30a5d816891c459c
Leonardo Kenji Yoshida <yoshidak.leonardo@outlook.com>
2018-02-23 02:04:19 -0300

    Finalizada aula de Commands

---
+++ Xamarin.App/Xamarin.App/Xamarin.App/ViewModel/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Xamarin.App.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        protected virtual void OnPropertyChanged([CallerMemberName]string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- Xamarin.App/Xamarin.App/Xamarin.App/ViewModel/MainViewModel.cs
+++ Xamarin.App/Xamarin.App/Xamarin.App/ViewModel/MainViewModel.cs
@@ -1,11 +1,42 @@
-﻿namespace Xamarin.App.ViewModel
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin.App.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
         public MainViewModel()
         {
-            this.Descricao = "Maratona Xamarin!! Welcome";
+            this.SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
         }
-        public string Descricao { get; set; }
+
+        private bool CanExecuteSearchCommand(object arg)
+        {
+            return !String.IsNullOrWhiteSpace(_searchTerm);
+        }
+
+        async void ExecuteSearchCommand(object obj)
+        {
+            await Task.Delay(2000);
+
+           await App.Current.MainPage.DisplayAlert("EstacioneAqui", $"Você pesquisou por: {_searchTerm}", "Ok");
+        }
+
+        private string _searchTerm;
+
+        public string SeachTerm
+        {
+            get { return _searchTerm; }
+            set
+            {
+                if (SetProperty(ref _searchTerm, value))
+                    this.SearchCommand.ChangeCanExecute();
+            }
+        }
+
+        public Command SearchCommand { get; }
+
     }
 }
